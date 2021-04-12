6bcd994830c9af1b06dee6c462f9d1031348f37f
charsh1re <lyjshow200@gmail.com>
2021-04-12 16:41:15 +0900

    b_insertion

--- sort/insert/insert_bi_sort.c
+++ sort/insert/insert_bi_sort.c
@@ -4,18 +4,33 @@
 #include <time.h>
 #define ARR_SIZE 3000
 
-void	insertion_sort(int *arr)
+int		bi_search(int *arr, int key, int l, int h)
+{
+	int mean;
+	mean = (l + h) / 2;
+	if (h <= l)
+		return (key > arr[l])? (l + 1): l;
+	if (key == arr[mean])
+		return (++mean);
+	if (key > arr[mean])
+		return (bi_search(arr, key, ++mean, h));
+	return (bi_search(arr,key,l,--mean));
+}
+
+void	bi_insertion_sort(int *arr)
 {
 	int key;
 	int i;
 	int j;
+	int l_bound;
 
 	i = 1;
 	while (i < ARR_SIZE)
 	{
 		key = arr[i]; //최초 key값
 		j = i - 1;
-		while (j >= 0 && arr[j] > key) // 처음 까지 가거나, 정렬 안된걸 >>로 밀음
+		l_bound = bi_search(arr, key, 0, j);
+		while (j >= l_bound) // 처음 까지 가거나, 정렬 안된걸 >>로 밀음
 		{
 			arr[j + 1] = arr[j]; // >> 이동
 			j--;
@@ -36,14 +51,14 @@ int		main()
 		arr[i] = rand() % 20000 + 1;
 	printf("origin arr is \n");
 	for (int i = 0; i<ARR_SIZE; i++)
-		printf("%dth element is %d\n", i, arr[i]);	
+		printf("%dth element is %d\n", i, arr[i]);
 	start = clock();
-	insertion_sort(arr);
+	bi_insertion_sort(arr);
 	end = clock();
-	res = (double)(end - start);
+	res = (double)(end-start);
 	printf("after sort\n");
 	for (int i = 0; i<ARR_SIZE; i++)
 		printf("%dth element is %d\n", i, arr[i]);	
 	free(arr);
-	printf("time taken:%f", res);
+	printf("time taken: %f",res);
 }
